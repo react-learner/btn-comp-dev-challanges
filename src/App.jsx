258890c22b85a984ca33da1bd6a015e286cc1c67
React Learner <suhashsai1999@gmail.com>
2022-09-09 20:19:56 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import ButtonsContainer from './Components/ButtonsContainer'
+import Footer from './Components/Footer'
 import SideBar from './Components/SideBar'
 
 function App() {
   
 
   return (
+    <div className='app_container' >
     <div className="App">
-      <SideBar/>
-      <ButtonsContainer/>
+    <SideBar/>
+    <ButtonsContainer/>
+    </div>
+    <Footer/>
     </div>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer' >
+    <h3>created by <span>react-learner</span> - devChallenges.io</h3>
+    </div>
+  )
+}
+
+export default Footer
